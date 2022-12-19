dda92d06433ca088cd63e079ceb335091cfb6dd4
Aniq Javed <2aniqqjavedd4493@gmail.com>
2022-12-19 13:12:43 +0500

    Add Workload model

---
+++ api/models/Workload.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const WorkloadSchema = new mongoose.Schema(
+    {
+        semester: {type: Number},
+        year: {type: Number},
+        employeeName: {type: String},
+        managerialResponsibility: {type: String}, //We source as array from other schema, out of which only field will be choosen
+        noOfStudents: {type: Number},
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        courseDetails: [{
+            courseId: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+                ref: 'Course'
+            },
+            courseContribution: {
+                type: Number
+            }                        
+        }],
+        research: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Research'
+        },
+    },
+    {timestamps: true}
+);
+
+module.exports = mongoose.model("Workload", WorkloadSchema)
