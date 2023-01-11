acde987c8459694b78ffc7cdca84e0ba090f073a
Aniq Javed <2aniqqjavedd4493@gmail.com>
2023-01-12 01:22:43 +0500

    Add User Route for Delete User

--- api/routes/users.js
+++ api/routes/users.js
@@ -37,6 +37,31 @@ router.put("/:id", verifyToken ,async (req,res)=>{
 })
 
 
+//DELETE USER
+
+router.delete("/:id", verifyToken ,async (req,res)=>{
+    if(req.user.id === req.params.id || req.user.isAdmin){
+        if(req.body.password){                     // I think there is no need of this section of code.
+            req.body.password = CryptoJS.AES.encrypt(
+                req.body.password,
+                process.env.SECRET_KEY
+            ).toString();
+        }
+
+        try{
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("User has been deleted.....")
+        }
+        catch(err){
+            res.status(500).json(err)
+        }
+    }
+
+    else{
+        res.status(403).json("You can delete only your account!");
+    }
+})
+
 
 
 
