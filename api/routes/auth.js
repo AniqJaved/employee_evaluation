9297d866cd2ff5af5ccbd9ae255c53802084a1ed
Aniq Javed <2aniqqjavedd4493@gmail.com>
2023-01-12 01:14:22 +0500

    Add Auth Route for User Login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -22,4 +22,33 @@ router.post("/register", async (req,res)=>{
 
 
 
+//LOGIN
+router.post("/login", async (req,res)=>{
+    try{
+        const user = await User.findOne({email: req.body.email});
+        !user && res.status(401).json("Wrong password or username!")
+
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const originalPassworrd = bytes.toString(CryptoJS.enc.Utf8);
+
+        originalPassworrd !== req.body.password &&
+        res.status(401).json("Wrong password or username!")
+
+        const accessToken = jwt.sign(
+            {id: user._id, isAdmin: user.isAdmin, isVisiting: user.isVisiting},  // It will return these fields in the jwt token.
+            process.env.SECRET_KEY,
+            { expiresIn: "5d"}
+        );
+
+        const { password, ...info} = user._doc; // So here we are using object destructuring in which we are grabbing password and only sending other fields except password in response. _doc is basically all the information retrieved about a single user.
+
+        res.status(200).json({...info, accessToken});
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
+
+
+
 module.exports = router
