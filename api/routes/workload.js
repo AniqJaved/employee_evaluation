509a95e7e1ae9f5bc56c7673692dcc9ba8bf3f9d
Aniq Javed <2aniqqjavedd4493@gmail.com>
2023-01-14 19:58:40 +0500

    Get workload of all users

--- api/routes/workload.js
+++ api/routes/workload.js
@@ -36,6 +36,26 @@ router.get("/find/:id", async (req,res)=>{
 })
 
 
+// GET ALL WORKLOAD
+router.get("/", verifyToken ,async (req,res)=>{
+    const query = req.query.new;  //By query we mean users?new=true
+    
+    if(req.user.isAdmin){
+        try{
+            const workloads = query ? await Workload.find().sort({_id:-1}).limit(10) : await Workload.find(); //sort({_id:-1}) it will sort from last to first and will return the latest data.
+            res.status(200).json(workloads)
+        }
+        catch(err){
+            res.status(500).json(err)
+        }
+    }
+
+    else{
+        res.status(403).json("You are not allowed to see all workloads!");
+    }
+})
+
+
 
 
 module.exports = router
